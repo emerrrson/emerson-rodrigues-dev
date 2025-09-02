b4d3325b992be4001a29d941463ae717bc999e4c
emerrrson <emerson188283@gmail.com>
2025-09-02 15:21:03 -0300

    adicionei mais um carrossel de imagens dinâmicas com javascript, e alterei algumas informações pessoais.

--- README.md
+++
@@ -1,1 +0,0 @@
-# emerson-rodrigues-dev
--- script.js
+++ script.js
@@ -88,3 +88,38 @@ botaoGithub.addEventListener('click', function (e) {
   e.preventDefault();
   alert("Ainda não subi esse projeto para o Github, segura aí!");
 })
+
+
+const items = document.querySelectorAll(".carousel-item");
+let current = 0;
+
+function updateCarousel() {
+  items.forEach((item, index) => {
+    item.classList.remove("active", "prev", "next");
+
+    if (index === current) {
+      item.classList.add("active");
+    } else if (index === (current - 1 + items.length) % items.length) {
+      item.classList.add("prev");
+    } else if (index === (current + 1) % items.length) {
+      item.classList.add("next");
+    }
+  });
+}
+
+document.getElementById("prev").addEventListener("click", () => {
+  current = (current - 1 + items.length) % items.length;
+  updateCarousel();
+});
+
+document.getElementById("next").addEventListener("click", () => {
+  current = (current + 1) % items.length;
+  updateCarousel();
+});
+
+// Inicializa
+updateCarousel();
+
+document.getElementById("re-site").addEventListener("click", () => {
+  alert("Em breve")
+})
